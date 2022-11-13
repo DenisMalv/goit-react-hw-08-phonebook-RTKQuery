beef59c60ad5c4c0046028aa21038f5848051d37
DenisMalv <kissed03@gmail.com>
2022-11-13 17:33:43 +0200

    add links

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,5 +1,6 @@
 import { useSelector } from 'react-redux';
 import { getToken } from 'redux/tokenSlice/tokenSlice';
+import { Link} from 'react-router-dom';
 
 const HomePage = () => {
   const isLogin = useSelector(getToken);
@@ -8,7 +9,7 @@ const HomePage = () => {
       <h1 style={{ textAlign: 'center' }}>HOMEPAGE </h1>
       {!isLogin && (
         <p style={{ textAlign: 'center', fontSize: 24 }}>
-          Please logIn for more...
+          Please <Link to='/login'>Log In</Link> for more access, or <Link to='/register'>Sign In</Link>.
         </p>
       )}
       {isLogin && (
